dc2c018091bb8e650cf38bd9442bf853d9a34231
gdrffg <gdrffg@gmail.com>
2024-03-29 14:31:25 +0900

    Create [PGS]할인행사_2.py

---
+++ hash/[PGS]할인행사_2.py
@@ -0,0 +1,25 @@
+def solution(want,number,discount):
+    # want 리스트를 딕셔너리로 변환
+    want_dict = {}
+    for i in range(len(want)):
+        want_dict[want[i]] = number[i]
+
+    answer = 0
+
+    for i in range(len(discount) - 9):
+        discount_10d = {}
+
+
+        for j in range(i, i+10):
+            if discount[j] in want_dict:
+                discount_10d[discount[j]] = discount_10d.get(discount[j],0) + 1
+
+        if discount_10d == want_dict:
+            answer += 1
+
+    return answer
+
+want = ["banana", "apple", "rice", "pork", "pot"]
+number = [3, 2, 2, 2, 1]
+discount = ["chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"]
+print(solution(want,number,discount))
